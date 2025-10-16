f5393d8289b7b533bb89e8a1ce97b7a4fa0eb536
Yobel Christian <finn.mcmissile259@gmail.com>
2025-10-16 21:42:01 +0700

    Update coba 1.py

--- coba 1.py
+++ coba 1.py
@@ -4,25 +4,25 @@ ukuran_baju = input("Masukkan Ukuran baju [XL/L/M/S] : ")
 if kode_baju == "AD" or kode_baju == "ad" :
     merk = "ADIDAS"
     if ukuran_baju == "XL" or ukuran_baju == "xl":
-        harga = 300000
-    elif ukuran_baju == "L" or ukuran_baju == "l":
-        harga = 250000
-    elif ukuran_baju == "M" or ukuran_baju == "m":
-        harga = 200000
-    elif ukuran_baju == "S" or ukuran_baju == "s":
         harga = 150000
+    elif ukuran_baju == "L" or ukuran_baju == "l":
+        harga = 130000
+    elif ukuran_baju == "M" or ukuran_baju == "m":
+        harga = 100000
+    elif ukuran_baju == "S" or ukuran_baju == "s":
+        harga = 60000
     else:
         harga = "Anda salah input ukuran"
 elif kode_baju == "NVD" or kode_baju == "nvd":
     merk = "NEVADA"
     if ukuran_baju == "XL" or ukuran_baju == "xl":
-        harga = 300000
+        harga = 100000
     elif ukuran_baju == "L" or ukuran_baju == "l":
-        harga = 280000
+        harga = 90000
     elif ukuran_baju == "M" or ukuran_baju == "m":
-        harga = 250000
+        harga = 70000
     elif ukuran_baju == "S" or ukuran_baju == "s":
-        harga = 210000
+        harga = 400000
     else:
         harga = "Anda salah input ukuran"
 else :
@@ -31,6 +31,6 @@ else :
 
 print("----------------------------")
 print("Merk Baju : " +str(merk))
-print("Ukuran : " +str(ukuran_baju))
-print("Harga :",harga)
+print("Ukuran    : " +str(ukuran_baju))
+print("Harga     :",harga)
 print("----------------------------")
